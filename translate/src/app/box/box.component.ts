6fb8ebcf676aa6a50fa00655ac14766bd39a3a53
andbardii <andbardii@icloud.com>
2023-10-05 14:33:08 +0200

    v1.0.0

--- translate/src/app/box/box.component.ts
+++ translate/src/app/box/box.component.ts
@@ -24,6 +24,13 @@ export class BoxComponent implements OnInit{
   }
 
   translate(){
-
-  }
+    this.svc.translate(this.first).subscribe(
+      (data) => {
+        console.log(data)
+        this.second = Object.values(data)[0].translations[0].translatedText
+      },
+      (err) => {
+        console.log(err)
+      }
+  )}
 }
--- translate/src/app/services/transalte.service.ts
+++ translate/src/app/services/transalte.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -9,29 +9,22 @@ export class TransalteService {
   first:string = ''
   second:string = ''
 
-  headers = new HttpHeaders();
-  params = new URLSearchParams({
-		q: '',
-		target: 'en',
-		source: 'it'
-	})
-
   constructor(private http:HttpClient) {
-    this.setHeaders()
-  }
-
-  setHeaders(){
-    this.headers = this.headers.set('X-RapidAPI-Host', 'google-translate1.p.rapidapi.com')
-                               .set('X-RapidAPI-Key', 'f4620064b9mshdb8d1bb25f138ebp15508ajsn932fd197513a');
-  }
-
-  setParams(first:string){
-    return this.params.set('q', first )
   }
 
   translate(first:string){
-    return this.http.post('https://google-translate1.p.rapidapi.com/language/translate/v2', this.setParams(first) , {
-      headers: this.headers
+    const headers = new HttpHeaders({
+      'content-type': 'application/x-www-form-urlencoded',
+      'X-RapidAPI-Key': 'f4620064b9mshdb8d1bb25f138ebp15508ajsn932fd197513a',
+      'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com',
+    });
+    const body = new URLSearchParams({
+      q: first,
+      target: 'en',
+      source: 'it',
+    }).toString();
+    return this.http.post('https://google-translate1.p.rapidapi.com/language/translate/v2', body , {
+      headers
     });
   }
 }
